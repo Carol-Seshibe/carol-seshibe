ab2a1d909a13d6c2e29ac4806cb735e1b802f9c2
Carol-Seshibe <carol.seshibe@gmail.com>
2025-06-02 19:41:38 +0000

    Small Changes

--- Java/Script.js
+++ Java/Script.js
@@ -13,14 +13,5 @@ window.addEventListener('load', () => {
     document.getElementById('MenuButton').addEventListener('click', function () {
         document.getElementById('NavMenu').classList.toggle('active');
     });
-    
-    videoIframe = document.querySelector('.EmbeddedVideo > iframe');
-    window.addEventListener('resize', resizeVideo);
-    resizeVideo();
+  resizeVideo();
 })
-
-function resizeVideo() {
-    const rect = document.body.getClientRects()[0];
-    videoIframe.setAttribute('width', rect.width / 2);
-    videoIframe.setAttribute('height', rect.width / 2 * videoApspectRatio)
-}
